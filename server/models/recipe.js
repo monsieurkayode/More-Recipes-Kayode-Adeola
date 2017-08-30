aafdb08f84fb7e1e03ef0597c16f5aaf0498a5b5
Kayode Adeola <monsieurkayode@gmail.com>
2017-08-30 12:22:32 +0100

    Refactor recipe model

--- server/models/recipe.js
+++ server/models/recipe.js
@@ -1,15 +1,39 @@
-'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var Recipe = sequelize.define('Recipe', {
-    title: DataTypes.STRING,
-    ingredients: DataTypes.STRING,
-    instructions: DataTypes.STRING
+const recipeModel = (sequelize, DataTypes) => {
+  const Recipe = sequelize.define('Recipe', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [3, 50],
+          msg: 'Title must be minimum 3 and maximum 50 characters'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Title cannot be empty'
+        },
+        is: {
+          args: /^[A-Za-z0-9]+$/gi,
+          msg: 'Title must contain letter and numbers only',
+        }
+      }
+    },
+    ingredients: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    instructions: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     classMethods: {
-      associate: function(models) {
+      associate: (models) => {
         // associations can be defined here
       }
     }
   });
   return Recipe;
 };
+
+export default recipeModel;
