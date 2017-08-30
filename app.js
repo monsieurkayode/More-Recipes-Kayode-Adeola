3721fdbd113ad77a4fb8622270fa59cc61e63f45
Kayode Adeola <monsieurkayode@gmail.com>
2017-08-30 16:51:09 +0100

    Import route in express application

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import router from './server/routes/index';
+
+const userRoute = router.user;
 
 // Set up the express app
 const app = express();
@@ -13,8 +16,9 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.disable('x-powered-by');
-
 app.use(express.static('template'));
+
+app.use(userRoute);
 
 app.get('/api', (req, res) => {
   res.status(200).send({
