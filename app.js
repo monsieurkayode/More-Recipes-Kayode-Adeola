73b0ac3fad0eafdfdfe4cd4a9a1cefdf607f4d9a
Kayode Adeola <monsieurkayode@gmail.com>
2017-08-30 09:26:02 +0100

    Setup application in app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
+// Set up the express app
+const app = express();
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.disable('x-powered-by');
+
+app.use(express.static('template'));
+
+app.get('/api', (req, res) => {
+  res.status(200).send({
+    message: 'Status connected ok',
+  });
+});
+
+app.get('*', (req, res) => res.status(404).send({
+  message: 'No page found',
+}));
+
+export default app;
