53fcd4ed37658e80ef84147b78089a2820767615
Kayode Adeola <monsieurkayode@gmail.com>
2017-08-30 12:11:47 +0100

    Refactor user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,57 @@
-'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+const userModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: 'Username already exists',
+      },
+      validate: {
+        len: {
+          args: [3, 30],
+          msg: 'Username must be minimum 3 and maximum 30'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Username cannot be empty'
+        },
+        is: {
+          args: /^[A-Za-z0-9]+$/gi,
+          msg: 'Username must contain letter and numbers only',
+        }
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: 'Email already exists',
+      },
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Email cannot be empty'
+        },
+        isEmail: {
+          arg: true,
+          msg: 'Invalid email address',
+        },
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   }, {
     classMethods: {
-      associate: function(models) {
+      associate(models) {
         // associations can be defined here
       }
     }
   });
   return User;
 };
+
+export default userModel;
