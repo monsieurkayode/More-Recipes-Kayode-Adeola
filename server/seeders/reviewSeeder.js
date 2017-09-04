8f430ecd1797f9af3006858276ef5e6ed8c96f34
Kayode Adeola <monsieurkayode@gmail.com>
2017-09-04 14:29:23 +0100

    Create seeders for favorite, reviews

---
+++ server/seeders/reviewSeeder.js
@@ -0,0 +1,13 @@
+const reviews = [
+  {
+    comment: 'This is an awesome recipe',
+  },
+  {
+    comment: 'It would be nice if you could throw in some ginger',
+  },
+  {
+    comment: 'Pretty please can I have some?',
+  }
+];
+
+export default reviews;
