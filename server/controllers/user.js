8874f1bc9e15a853824674cb05c4dd46017a5012
Kayode Adeola <monsieurkayode@gmail.com>
2017-09-05 14:44:33 +0100

    Remove jwt implementation at users signup endpoint

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,22 +1,14 @@
 import dotenv from 'dotenv';
-import jwt from 'jsonwebtoken';
 import db from '../models/index';
 
 dotenv.load();
 const User = db.User;
-const secret = process.env.secretKey;
 
 const userController = {
   signup(req, res) {
     return User
       .create(req.body, { fields: Object.keys(req.body) })
       .then((user) => {
-        const token = jwt.sign(
-          { userId: user.id,
-            username: user.username,
-            email: user.email,
-          }, secret
-        );
         res.status(201).send({
           id: user.id,
           username: user.username,
