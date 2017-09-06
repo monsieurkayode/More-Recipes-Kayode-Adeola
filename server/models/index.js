32e6f991558e3eba8bc5784bc7a5e70e346d8b5b
Kayode Adeola <monsieurkayode@gmail.com>
2017-09-06 18:53:07 +0100

    Modify db config

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import path from 'path';
+import dotenv from 'dotenv';
 import Sequelize from 'sequelize';
 import dbConfig from '../config/config.json';
 
+dotenv.config();
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
 const config = dbConfig[env];
@@ -10,7 +12,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(config.use_env_variable);
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(
     config.database, config.username, config.password, config
