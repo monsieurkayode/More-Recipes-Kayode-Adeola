ef2471677103952c8fef6ccfc01c115aacd68ddf
Kayode Adeola <monsieurkayode@gmail.com>
2017-09-09 10:26:38 +0100

    Comment code

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,20 +1,33 @@
+// import module dependencies
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import errorHandler from '../helpers/responseHandler';
 
+// Load environment variables
 dotenv.load();
 const secret = process.env.secretKey;
 
+/**
+ * 
+ * @param {object} req http request object to server
+ * @param {object} res http response object from server
+ * @param {function} next 
+ * @returns {object} status message
+ */
 const auth = (req, res, next) => {
+  // Reference token
   const token = req.body.token || req.query.token || req.headers['x-access-token'];
+  // Authenticate token if provided
   if (token) {
     jwt.verify(token, secret, (err, decoded) => {
       if (err) {
+        // If token authentication fails due to expiration
         if (err.name === 'TokenExpiredError') {
           return errorHandler(
             403, 'Your session has expired, sign in again', res
           );
         }
+        // Return error if token is bad
         return errorHandler(
           403, 'Bad Token', res
         );
