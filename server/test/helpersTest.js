8aa9666c8d7f393fdff7b84c8ff382dc7e184fbc
Kayode Adeola <monsieurkayode@gmail.com>
2017-09-15 06:57:50 +0100

    Test helper functions

--- server/test/helpersTest.js
+++ server/test/helpersTest.js
@@ -1,8 +1,36 @@
 import { expect } from 'chai';
 import cleanString from '../helpers/cleanString';
+import isEmail from '../helpers/isEmail';
+import isEmpty from '../helpers/isEmpty';
+import isNumber from '../helpers/isNumber';
+import isAlphaNum from '../helpers/isAlphaNum';
 
 describe('Test helpers', () => {
-  it('should remove whitespaces', () => {
+  it('cleanString(ab d) should equal abd', () => {
     expect(cleanString('ab d')).to.equal('abd');
+  });
+  it('isEmail(jay@.com) should be false', () => {
+    expect(isEmail('jay@.com')).to.be.equal(false);
+  });
+  it('isEmail(jay@gmail.com) should be true', () => {
+    expect(isEmail('jay@gmail.com')).to.be.equal(true);
+  });
+  it('isEmpty( ) should be true', () => {
+    expect(isEmpty(' ')).to.be.equal(true);
+  });
+  it('isEmpty(not) should be false', () => {
+    expect(isEmpty('not')).to.be.equal(false);
+  });
+  it('isNumber(2) should be true', () => {
+    expect(isNumber('2')).to.be.equal(true);
+  });
+  it('isNumber(alpha) should be false', () => {
+    expect(isNumber('alpha')).to.be.equal(false);
+  });
+  it('isAlphaNum(%^) should be false', () => {
+    expect(isAlphaNum('%^')).to.be.equal(false);
+  });
+  it('isAlphaNum(bond007) should be true', () => {
+    expect(isAlphaNum('bond007')).to.be.equal(true);
   });
 });
