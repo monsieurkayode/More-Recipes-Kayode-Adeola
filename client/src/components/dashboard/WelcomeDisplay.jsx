a63da3e1cd0c5ad3ded73cff5924cda0b3e2d99e
Kayode Adeola <monsieurkayode@gmail.com>
2017-09-30 12:40:48 +0100

    Add welcome display component for user dashboard page

---
+++ client/src/components/dashboard/WelcomeDisplay.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class WelcomeDisplay extends Component {
+  render() {
+    return (
+      <div class="card-panel grey lighten-4">
+        <span class="card-content">
+          <h5>Welcome Kayode!</h5>
+          <span>Dashboard/My Recipes</span>
+        </span>
+      </div>
+    );
+  }
+}
+
+export default WelcomeDisplay;
