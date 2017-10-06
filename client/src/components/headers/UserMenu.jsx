decced4bc64814a16b804bafb505f65465414a9e
Kayode Adeola <monsieurkayode@gmail.com>
2017-10-06 12:38:40 +0100

    Link page views

--- client/src/components/headers/UserMenu.jsx
+++ client/src/components/headers/UserMenu.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const UserMenu = (props => {
   return(
     <ul id="user-dropdown" className="dropdown-content">
       <li><a className="modal-trigger" href="#modal-newpost"><span className="fa fa-lg fa-pencil-square-o"></span> New Post</a></li>
-      <li><a href="dashboard.html"><span className="fa fa-lg fa-wrench"></span> Dashboard</a></li>
+      <li><Link to="/dashboard"><span className="fa fa-lg fa-wrench"></span> Dashboard</Link></li>
       <li className="divider"></li>
-      <li><a href="index.html"><span className="fa fa-lg fa-lock"></span> Logout</a></li>
+      <li><Link to="/"><span className="fa fa-lg fa-lock"></span> Logout</Link></li>
     </ul>
   )
 });
