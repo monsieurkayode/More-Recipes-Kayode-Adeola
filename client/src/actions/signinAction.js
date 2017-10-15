73f138265548d0cbb70172b70fddd765baa8bec6
Kayode Adeola <monsieurkayode@gmail.com>
2017-10-15 03:32:12 +0100

    Make signin action creator and reducer

---
+++ client/src/actions/signinAction.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const SIGNIN_SUCCESSFUL = 'SIGNIN_SUCCESSFUL';
+export const SIGNIN_UNSUCCESSFUL = 'SIGNIN_UNSUCCESSFUL';
+
+const signinAction = (user) => dispatch => {
+  axios.post('/api/v1/users/signin', user)
+    .then((response) => {
+    if (response.status === 200) {
+      const { Token } = response.data;
+      localStorage.setItem('token', Token);
+      dispatch({ type:SIGNIN_SUCCESSFUL });
+    }
+  })
+  .catch((error) => {
+    if (error.response && error.response.status >= 400 ) {
+      const { message } = error.response.data
+      dispatch({ type:SIGNIN_UNSUCCESSFUL, payload: message });
+    }
+  })
+}
+
+export default signinAction;
