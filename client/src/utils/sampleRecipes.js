e6f89b5aa6c988c18475f543e94babd51e15439d
Kayode Adeola <monsieurkayode@gmail.com>
2017-10-26 10:09:45 +0100

    Create sample recipes to be loaded in application

---
+++ client/src/utils/sampleRecipes.js
@@ -0,0 +1,49 @@
+export default {
+  recipe1: {
+    id: 1,
+    views: 16,
+    upvotes: 3,
+    downvotes: 0,
+    recipeName: 'Spice',
+    category: 'Spice',
+    ingredients: '',
+    instructions: '',
+    imageUrl: '../css/img/spice.jpg'
+  },
+
+  recipe2: {
+    id: 2,
+    views: 14,
+    upvotes: 3,
+    downvotes: 9,
+    recipeName: 'Hamburger',
+    category: 'Fast Food',
+    ingredients: '',
+    instructions: '',
+    imageUrl: '../css/img/burger.jpg'
+  },
+
+  recipe3: {
+    id: 3,
+    views: 32,
+    upvotes: 20,
+    downvotes: 11,
+    recipeName: 'Tuna Fish',
+    category: 'SeaFood',
+    ingredients: '',
+    instructions: '',
+    imageUrl: '../css/img/fishmeal.jpg'
+  },
+
+  recipe4 : {
+    id: 4,
+    views: 23,
+    upvotes: 12,
+    downvotes: 7,
+    recipeName: 'Frosty Chocolat',
+    category: 'Pastry',
+    ingredients: '',
+    instructions: '',
+    imageUrl: '../css/img/cake2.jpg'
+  },
+};
