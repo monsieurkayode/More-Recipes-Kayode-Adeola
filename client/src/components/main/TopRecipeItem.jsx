010de06f1a24d1d181730b5ccc64e663dce814a8
Kayode Adeola <monsieurkayode@gmail.com>
2017-11-03 21:24:14 +0100

    Get image file path

--- client/src/components/main/TopRecipeItem.jsx
+++ client/src/components/main/TopRecipeItem.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 
 const TopRecipeItem = props => 
   <Link to=''><li className="collection-item avatar">
-    <img src={`../uploads/${props.recipe.image}`} alt="" className="z-depth-1 square" />
+    <img 
+      src={props.recipe.image ? `../uploads/${props.recipe.image}` : '../css/img/spice.jpg'} 
+      alt="" className="z-depth-1 square" />
     <span className="title red-text">{props.recipe.recipeName}</span>
   </li></Link>
 
