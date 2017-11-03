78d1e7df0f5b34fc201e3e3f70b8d20f287a5e12
Kayode Adeola <monsieurkayode@gmail.com>
2017-11-03 21:10:30 +0100

    Make use of formData for mulipart form data in creating recipe post

--- client/src/actions/createPost.js
+++ client/src/actions/createPost.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 import actionTypes from './actionTypes';
 
-const createPost = (values, callback) => dispatch =>
-  axios.post('/api/v1/recipes', values)
+const createPost = (values, callback) => dispatch => {
+  const formData = new FormData();
+  formData.append('recipeName', values.recipeName);
+  formData.append('category', values.category);
+  formData.append('ingredients', values.ingredients);
+  formData.append('instructions', values.instructions);
+  formData.append('image', values.image ? values.image['file'] : 'spice.jpg');
+  return axios.post('/api/v1/recipes', formData)
     .then(({ data }) => {
       dispatch({type:actionTypes.CREATE_POST, payload: data })
       callback();
     })
-
+}
 export default createPost;
