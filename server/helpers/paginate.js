79cdc2626b2f9c19b9503350dbed4829dfbfb608
Kayode Adeola <monsieurkayode@gmail.com>
2017-11-13 23:43:46 +0100

    Create helper function to validate pagination query

--- server/helpers/paginate.js
+++ server/helpers/paginate.js
@@ -1,9 +1,19 @@
 const paginate = (page, limit, result) => [{
   page: parseInt(page, 10),
   pageCount: Math.ceil(result.count / limit),
-  pageSize: parseInt(limit, 10),
+  pageSize: result.rows.length,
   totalCount: result.count },
 result.rows
 ];
 
-export default paginate;
+const validatePaginate = (req) => {
+  const page = Number.isInteger(parseInt(req.query.page, 10))
+  && req.query.page > 0 ? req.query.page : 1;
+  const limit = Number.isInteger(parseInt(req.query.limit, 10))
+  && req.query.limit > 0 ? req.query.limit : 5;
+  const offset = (page - 1) * limit;
+
+  return { page, limit, offset };
+};
+
+export { paginate, validatePaginate };
